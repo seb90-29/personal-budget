dc1f41ce77a3c72366cab37f51a58b1a06b69928
seb90-29 <sswientulski@gmail.com>
2024-02-25 17:31:22 +0100

    GET money, envelopes, charges added.

--- envelopeRouter.js
+++ envelopeRouter.js
@@ -16,59 +16,86 @@ const {
 
 const pool = require('./databasepg.js')
 
+envelopeRouter.use(bodyParser.json())
+
+//GET unenveloped money
+
   envelopeRouter.get('/envelopes/budget/unenvelopedMoney', async (req, res) => {
     try {
-      const result = await pool.query('SELECT SUM(value) FROM unenveloped_money');
-      res.json(result.rows);
-    } catch (err) {
+      const unenvelopedMoney = await pool.query('SELECT SUM(value) AS "unenveloped money" FROM unenveloped_money');
+      res.status(200).send({
+        status: 'Sucess.',
+        message: 'Uneveloped money information provided.',
+        data: unenvelopedMoney.rows
+      })} 
+      catch (err) {
       console.error(err);
-      res.status(404).send('Table does not exist.');
+      res.status(404).send('No money has been added.');
     }
   });
 
-envelopeRouter.use(bodyParser.json())
+  // GET All envelopes
+
+  envelopeRouter.get('/envelopes', async (req, res) => {
+    try {
+      const allEnvelopes = await pool.query('SELECT * FROM envelopes');
+      res.status(200).send({
+        status: 'Sucess.',
+        message: 'All envelopes provided.',
+        data: allEnvelopes.rows
+      })} catch (err) {
+      console.error(err);
+      res.status(404).send('No envelopes are created.');
+    }
+  });
+
+//GET One envelope
+
+envelopeRouter.get('/envelopes/:id', async (req, res) => {
+  try {
+      const envelopeId = parseInt(req.params.id);
+      const oneEnvelope = await pool.query('SELECT * FROM envelopes WHERE envelope_id = $1', [envelopeId])
+      if (oneEnvelope.rowCount < 1) {
+          return res.status(404).send({
+              message: "Envelope does not exists."
+          });
+      };
+      res.status(200).send({
+          status: 'Sucess',
+          message: 'Envelope information provided.',
+          data: oneEnvelope.rows
+      });
+  } catch (error) {
+      console.error(error.message);
+  };
+});
+
+// GET envelope's charges
+
+envelopeRouter.get('/envelopes/charges/:id', async (req, res) => {
+  try {
+      const chargesId = parseInt(req.params.id);
+      const envelpesCharges = await pool.query('SELECT * FROM charges WHERE envelope_id = $1', [chargesId])
+      if (envelpesCharges.rowCount < 1) {
+          return res.status(404).send({
+              message: "There is no charges applied."
+          });
+      };
+      res.status(200).send({
+          status: 'Sucess',
+          message: 'Charges list provided.',
+          data: envelpesCharges.rows
+      });
+  } catch (error) {
+      console.error(error.message);
+  };
+});
 
 //GET total remaining
-envelopeRouter.get('/envelopes/budget/remaining', (req, res) => {
-  const totalRemainingBudget = "Total remaining budget in your envelopes: " + totalRemaining()
-  res.send(totalRemainingBudget)
-})
 
 //GET total budget
-envelopeRouter.get('/envelopes/budget', (req, res) => {
-  const entireBudget = "Total budget: " + getTotalBudget()
-  res.send(entireBudget)
-})
 
 //GET remaining budget
-envelopeRouter.get('/envelopes/budget/:id', (req, res) => {
-  const id = parseInt(req.params.id)
-  if (typeof id !== 'number') {
-    res.status(400).send('ID has to be a number.')
-  }else{
-  const envelopeRemainingBudget = "Remining budget in requested envelope: "+ getRemainingBudget(id)
-    res.status(200).send(envelopeRemainingBudget)
-}})
-
-//GET all
-envelopeRouter.get('/envelopes', (req, res) => {
-    const allEnvelopes = getEnvelopes()
-    res.send(allEnvelopes)
-})
-
-//GET one
-envelopeRouter.get('/envelopes/:id', (req, res, next) => {
-    const id = parseInt(req.params.id)
-    if (typeof id !== 'number') {
-      res.status(400).send('ID has to be a number.')
-    }else{
-    const envelope = getEnvelopeById(id)
-    if (envelope === -1){
-      res.status(404).send('Envelope ID not found!')
-    } else{
-      res.send(envelope)
-    }
-}})
 
 //POST new envelope
 envelopeRouter.post('/envelopes/new-envelope', (req, res, next) => {
